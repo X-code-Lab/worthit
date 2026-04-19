66329bbd6d0c2f865214f7c518890ac769609f39
X-code-Lab <rajatheroyal81@gmail.com>
2026-04-20 00:05:34 +0530

    Delete src directory

--- src/utils/calc.js
+++
@@ -1,5 +0,0 @@
-export const calcEMI = (P, r, m) => {
-  if (P <= 0 || m <= 0) return 0;
-  const i = r / 12 / 100;
-  return i === 0 ? P / m : (P * i * Math.pow(1 + i, m)) / (Math.pow(1 + i, m) - 1);
-};
