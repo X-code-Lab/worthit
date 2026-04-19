b817a1719db58a134b89ed17b13f32494599085c
X-code-Lab <rajatheroyal81@gmail.com>
2026-04-19 23:41:25 +0530

    Delete src/Badge.jsx

--- src/Badge.jsx
+++
@@ -1,7 +0,0 @@
-const Badge = ({ color, children }) => (
-  <span style={{ padding: "3px 10px", borderRadius: 20, background: `${color}18`, color, fontSize: 10, fontWeight: 700 }}>
-    {children}
-  </span>
-);
-
-export default Badge;
